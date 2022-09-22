4ca7d73e542dc46fef5961c4386273222fcff661
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-09-22 10:07:50 +0530

    Java microservice

---
+++ MicroservicespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicespringbootApplication.class, args);
+	}
+
+}
